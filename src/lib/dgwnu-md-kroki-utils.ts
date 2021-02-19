9a32fbc2fa3393edfbd2c56155f0a1e09d4a560a
Amitava Haldar <onno.haldar@icloud.com>
2021-02-20 00:35:17 +0100

    refactor

--- src/lib/dgwnu-md-kroki-utils.ts
+++ src/lib/dgwnu-md-kroki-utils.ts
@@ -5,8 +5,24 @@
 /**
  * Node Package Imports
  */
+import { readdirSync, existsSync, unlinkSync } from 'fs';
 
 /**
  * Local Library Imports
  */
 
+/**
+ * List all files that ends with .md 
+ * @param mdFilePath path naar de .md files
+ */
+export function listMdFiles(mdFilePath: string) {
+    let mdFiles: string[] = [];
+
+    readdirSync(mdFilePath).forEach(file => {
+        if (file.endsWith('.md')) {
+            mdFiles.push(file);
+        }
+    });
+
+    return mdFiles;
+}
