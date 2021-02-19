48e9c9d44b4fdc3932d12716bd38bbe06a58897c
Amitava Haldar <onno.haldar@icloud.com>
2021-02-20 00:47:39 +0100

    refactor

--- src/lib/dgwnu-md-kroki-utils.ts
+++ src/lib/dgwnu-md-kroki-utils.ts
@@ -6,10 +6,7 @@
  * Node Package Imports
  */
 import { readdirSync, existsSync, unlinkSync } from 'fs';
-
-/**
- * Local Library Imports
- */
+import { deflate } from 'pako';
 
 /**
  * List all files that ends with .md 
@@ -26,3 +23,15 @@ export function listMdFiles(mdFilePath: string) {
 
     return mdFiles;
 }
+
+/**
+ * Encode Diagram to make Roki Api request pay-load
+ * @param diagramSource 
+ */
+export function encodeDiagram(diagramSource: string) {
+    const data = Buffer.from(diagramSource, 'utf8');
+    const compressed = deflate(data, { level: 9 });
+    return Buffer.from(compressed)
+        .toString('base64') 
+        .replace(/\+/g, '-').replace(/\//g, '_');
+} 
