83d8288d0c9935dd7e1da29c8e22f49b2136c45b
Amitava Haldar <onno.haldar@icloud.com>
2021-02-20 01:01:25 +0100

    refactor

--- src/bin/md-pre-kroki-cli.ts
+++ src/bin/md-pre-kroki-cli.ts
@@ -8,35 +8,29 @@
  * Node Package Modules
  */
 import { argv } from 'process';
-import { inspect } from 'util';
-import { Observable, throwError } from 'rxjs';
-import * as Colors from 'colors';
+import { resolve } from 'path';
 
 /**
  * CLI Library Modules
  */
 import { 
+    listMdFiles
 } from '../lib/dgwnu-md-kroki-utils';
 
-/**
- * colors constant that is used for console.log()
- * Based on <https://www.voidcanvas.com/make-console-log-output-colorful-and-stylish-in-browser-node/>
- */
-const colors = Colors;
 
 //
 // START CLI Script
 //
-const command = argv[2];
-let parms: string[] = [];
 
-if (argv[3]) {
-    parms.push(argv[3]);
+if (argv.length != 4) {
+    console.error('2 parms required (inputDir outputDir)');
 }
 
-if (argv[4]) {
-    parms.push(argv[4]);
+const inputDir = argv[2];
+const outputDir = argv[3];
+
+const inputMdFiles = listMdFiles(resolve(__dirname, outputDir));
+
+for (const inputMdFile of inputMdFiles) {
+    console.log(inputMdFile);
 }
-
-console.log(`DGWNU - Fuseki CLI - ${command} ${parms}`.bgRed.yellow.bold);
-
