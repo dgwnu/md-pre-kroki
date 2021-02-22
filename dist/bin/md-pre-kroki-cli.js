4a1a163f6b440b112bb529ac2a2ed5cbe522f483
Amitava Haldar <onno.haldar@icloud.com>
2021-02-22 15:53:11 +0100

    refactor

--- dist/bin/md-pre-kroki-cli.js
+++ dist/bin/md-pre-kroki-cli.js
@@ -23,12 +23,18 @@ if (process_1.argv.length != 4) {
 const inputDir = path_1.resolve(process_1.argv[2]);
 const outputDir = path_1.resolve(process_1.argv[3]);
 console.log(inputDir);
-const inputMdFiles = lib_1.listMdFiles(inputDir);
-for (const inputMdFile of inputMdFiles) {
-    console.log(inputMdFile);
-    const basicMdStr = lib_1.preProcessKrokiMdFile(path_1.join(inputDir, inputMdFile));
-    console.log(basicMdStr);
-    const outputMdFile = path_1.join(outputDir, inputMdFile.split('.kroki.md')[0] + '.md');
-    console.log(outputMdFile);
+const mdFiles = lib_1.listMdFiles(inputDir);
+for (const mdFile of mdFiles) {
+    const inputMdFile = path_1.join(inputDir, mdFile);
+    console.log('='.repeat(40));
+    console.log(`Pre-Processing: ${inputMdFile}`);
+    const basicMdStr = lib_1.preProcessKrokiMdFile(inputMdFile);
+    console.log('Processed Kroki Api Inline Mark Down:');
+    console.log('-'.repeat(40));
+    console.log(`${basicMdStr}`);
+    console.log('-'.repeat(40));
+    const outputMdFile = path_1.join(outputDir, mdFile);
+    console.log(`Writing To File: ${outputMdFile}`);
     fs_1.writeFileSync(outputMdFile, basicMdStr);
+    console.log('='.repeat(40));
 }
